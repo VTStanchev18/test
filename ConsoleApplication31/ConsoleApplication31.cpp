b7eb19f7e60a331351b4499a6856fc5b5d099333
VTStanchev18 <VTStanchev18@codingburgas.bg>
2020-08-24 15:00:55 +0300

    Delete comments

--- ConsoleApplication31/ConsoleApplication31.cpp
+++ ConsoleApplication31/ConsoleApplication31.cpp
@@ -7,19 +7,6 @@ int main()
 {
     
 }
-// 
-//
-//zdrawejte!
-//
-// 
-//da tuk sum-magi <3   aaaaa ese ese ese
-// Ese  ese ese *mind blown*
-// (╯°□°）╯︵ ┻━┻
-// ┬─┬ ノ( ゜-゜ノ) opravih masata sori
-// Samo Barca!
-//napishete ese za da spechelite pari!!
-//ese meze
-//｡＾･ｪ･＾｡  meow <3 <3 <3
-//sa
+
 
 
