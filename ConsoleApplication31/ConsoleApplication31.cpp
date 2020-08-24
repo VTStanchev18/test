57079d2880291bc78968c3d76043414c9a5ed62f
VTStanchev18 <VTStanchev18@codingburgas.bg>
2020-08-24 14:57:36 +0300

    Add project files.

---
+++ ConsoleApplication31/ConsoleApplication31.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}
+// 
+//
+//zdrawejte!
+//
+// 
+//da tuk sum-magi <3   aaaaa ese ese ese
+// Ese  ese ese *mind blown*
+// (╯°□°）╯︵ ┻━┻
+// ┬─┬ ノ( ゜-゜ノ) opravih masata sori
+// Samo Barca!
+//napishete ese za da spechelite pari!!
+//ese meze
+//｡＾･ｪ･＾｡  meow <3 <3 <3
+//sa
+
+
